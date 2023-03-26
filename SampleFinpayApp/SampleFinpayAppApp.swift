c1d9a7fdf3027bfab0b2c24e99a7cb9a728ee555
Rasyid <rsyddn20@gmail.com>
2023-03-26 21:40:54 +0700

    initial commit

---
+++ SampleFinpayApp/SampleFinpayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFinpayAppApp.swift
+//  SampleFinpayApp
+//
+//  Created by Muhammad Rasyiddin on 18/03/23.
+//
+
+import SwiftUI
+import FinpaySDK
+@main
+struct SampleFinpayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PulsaDataView()
+        }
+    }
+}
